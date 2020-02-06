9c603e10e8beb830976564758ec5efc0eaec8396
joann129 <a0905187634@gmail.com>
2020-02-06 20:40:42 +0800

    UVA 11541 finished

--- UVA11541.c
+++ UVA11541.c
@@ -3,10 +3,22 @@
 #include <string.h>
 
  int main (void) {
- 	int t, i, n;
+ 	int t, i, n, j;
  	char c;
+ 	scanf("%d", &t);
+ 	getchar();
 	 for(i = 0; i < t; i++)	{
-	 	scanf(" %c%d", &c, &n);
-	 	
+	 	printf("Case %d: ", i+1);
+	 	while(1){
+	 		c = getchar();
+	 		if(c == '\n') {
+	 			printf("\n");
+	 			break;
+			 }
+	 		scanf("%d", &n);
+	 		for(j = 0; j < n; j++) {
+	 			printf("%c", c);
+			 }
+		 }
 	 }
  } 
