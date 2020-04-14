bd82b2c8594c9c3a97d90340baddf21d3f1fd456
joann129 <a0905187634@gmail.com>
2020-04-15 00:09:42 +0800

    UVA255 finished

--- UVA255.c
+++ UVA255.c
@@ -1,29 +1,63 @@
-#include <stdio.h>
 #include <stdlib.h>
-#define side 8
+#include <stdio.h>
 
-int arr[side][side];	
+int map[8][8];
+int dxy[4][2] = {0, 1, 0, -1, -1, 0, 1, 0};	//king能走為1 queen能走為2 兩者皆可為3 
 
-void create (void) {
+void create(void) {
 	int i, j;
-	for(i = 0; i < side; i++) {
-		for(j = 0; j < side; j++) {
-			arr[i][j] = 0;
-		} 
+	for(i = 0; i < 8; i++) {
+		for(j = 0; j < 8; j++) {
+			map[i][j] = 0;
+		}
+	}
+}
+
+void print(void) {
+	int i, j;
+	for(i = 0; i < 8; i++) {
+		for(j = 0; j < 8; j++) {
+			printf("%d ", map[i][j]);
+		}
+		printf("\n");
 	}
 }
 
 int main(void) {
-	int kingState, queenState, queenMove;
-	while(1) {
-		scanf("%d %d %d", &kingSate, &queenState, &queenMove);
-		if(kingState == queenState) {
-			printf("Illegal state\n");
-			continue;
-		}else{
-			if((queenMove - queenSate) % 8 == 0) {
-				
+	int k, q, m, i, x, y;
+	while(scanf("%d %d %d", &k, &q, &m) != EOF) {
+		create();//printf("1");
+		for(i = 0; i < 4; i++) {
+			x = k / 8 + dxy[i][0];
+			y = k % 8 + dxy[i][1];
+			if(x >= 0 && x <= 7 || y >= 0 && y <= 7) {
+				map[x][y] += 1;
+			}
+			x = q / 8;
+			y = q % 8;
+			while(1) {
+				x += dxy[i][0];
+				y += dxy[i][1];
+				if(x >= 0 && x <= 7 && y >= 0 && y <= 7 && 8*x+y != k) {
+					map[x][y] += 2;
+				}else{
+					break;
+				}
 			}
 		}
+		if(k == q) {
+			printf("Illegal state\n");
+		}else if(map[m/8][m%8] < 2 || m == k ) {
+			printf("Illegal move\n");
+		}else if(map[m/8][m%8] == 3) {
+			printf("Move not allowed\n");
+		}else if(k == 0 && m == 9 || k == 7 && m == 14 
+					|| k == 56 && m == 49 || k == 63 && m == 54) {
+			printf("Stop\n");
+		}else{
+			printf("Continue\n");
+		}
 	}
+	
+	
 }
